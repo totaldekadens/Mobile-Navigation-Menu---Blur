ee08676cfda7bc46e4b7b3ccbd2ce13abc3a9552
Angelica Moberg <90898648+totaldekadens@users.noreply.github.com>
2021-11-27 20:40:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelector(".burgerTarget").addEventListener("click", () => {
+        document.querySelector("body").classList.toggle("toggled")
+    })
+})
+
+
+
